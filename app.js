73570163d2d01f81f027d816200ccadf3fd233f2
Neo-dev-87 <stniosoares@yahoo.com.br>
2024-09-06 16:44:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById('resultados-pesquisa');
+
+  
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Constrói o HTML para cada item de resultado
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao} </p>
+          <a href="${dado.link}" target="_blank">Mais Informações</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+  }
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    {
+      titulo: "Hospital Felício Rocho",
+      descricao: "O Hospital Felício Rocho é uma referência em saúde em Minas Gerais e no Brasil. Localizado em Belo Horizonte, o hospital oferece uma ampla gama de especialidades médicas, além de realizar um grande número de cirurgias e transplantes anualmente. Fundado em 1942, o Felício Rocho é reconhecido por sua excelência em atendimento, corpo clínico altamente qualificado e infraestrutura moderna. A instituição também se destaca por sua atuação em ensino e pesquisa, contribuindo para o avanço da medicina no país.",
+      link: "https://www.feliciorocho.org.br/"
+    },
+    {
+      titulo: "Hospital Albert Einstein",
+      descricao: "O Hospital Albert Einstein é um dos maiores e mais completos complexos hospitalares da América Latina. Localizado em São Paulo, o Einstein oferece serviços de alta complexidade em diversas áreas da medicina, além de ser referência em pesquisa e ensino. O hospital é reconhecido internacionalmente por sua excelência em atendimento e por seus avanços tecnológicos.",
+      link: "https://www.einstein.br/"
+    },
+    {
+      titulo: "Hospital Sírio-Libanês",
+      descricao: "O Hospital Sírio-Libanês é uma instituição de saúde de referência nacional e internacional, com unidades em São Paulo e Brasília. O hospital oferece serviços de alta complexidade em diversas especialidades médicas, além de ser reconhecido por seus programas de ensino e pesquisa. O Sírio-Libanês é conhecido por sua excelência em atendimento e por seus avanços tecnológicos.",
+      link: "https://hospitalsiriolibanes.org.br/"
+    },
+    {
+      titulo: "Hospital Alemão Oswaldo Cruz ",
+      descricao: " Com uma longa história, o Alemão Oswaldo Cruz é conhecido por sua excelência em diversas especialidades, como neurologia e ortopedia. ",
+      link: "https://www.hospitaloswaldocruz.org.br/"
+    },
+    {
+      titulo: "Hospital Moinhos de Vento",
+      descricao: "Referência em cardiologia e transplantes no Sul do país.",
+      link: "https://www.hospitalmoinhos.org.br/institucional"
+    },
+    {
+      titulo: "Hospital das Clínicas da USP",
+      descricao: "Um dos maiores hospitais universitários da América Latina, com forte atuação em ensino e pesquisa.",
+      link: "http://www.hc.fm.usp.br/hc/portal/ "
+    },
+    {
+      titulo: "Hospital Samaritano",
+      descricao: "Reconhecido por sua excelência em ginecologia e obstetrícia.",
+      link: "https://www.hospitalsamaritano.com.br/"
+    },
+    {  
+      titulo: "Hospital Santa Marcelina",
+      descricao: "Com uma longa história, o Santa Marcelina oferece uma ampla gama de serviços, com destaque para a cardiologia e a neurologia.",
+      link: "https://santamarcelina.org/"
+    },
+    {
+      titulo: "Hospital São Rafael",
+      descricao: "Um dos maiores hospitais privados da Bahia, o São Rafael é referência em diversas especialidades, como oncologia e cardiologia.",
+      link: "https://www.rededorsaoluiz.com.br/hospital/sao-rafael "
+    },
+    {
+      titulo: "Hospital das Clínicas da UFPE",
+      descricao: "Importante centro de ensino e pesquisa, o HC-UFPE oferece atendimento de alta complexidade em diversas áreas.",
+      link: "https://www.ufpe.br/agencia/-/asset_publisher/dlhi8nsrz4hK/content/id/3245559"
+    },
+    {
+      titulo: "Hospital Evangélico de Curitiba",
+      descricao: "Um dos principais hospitais privados do Paraná, o Evangélico é referência em diversas especialidades, como oncologia e cardiologia.",
+      link: "https://pt.wikipedia.org/wiki/Hospital_Evang%C3%A9lico "
+    }
+  ];
